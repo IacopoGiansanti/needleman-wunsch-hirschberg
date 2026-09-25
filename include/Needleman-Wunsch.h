3ec37324d58b1ada7ed3b8594cbd78dafb19f659
IacopoGiansanti <iac.giansanti@stud.uniroma3.it>
2026-09-25 15:26:27 +0200

    removed useless header

--- include/Needleman-Wunsch.h
+++ include/Needleman-Wunsch.h
@@ -1,11 +1,9 @@
 #ifndef NEEDLEMAN_WUNSCH_H
 #define NEEDLEMAN_WUNSCH_H
 
-#include "base.h"
 #include "strand.h"
 #include "scoring.h"
 #include "alignment.h"
-#include "direction.h"
 
 Alignment needleman_wunsch(const Strand *v, const Strand *c, const scoringModel *model);
 
